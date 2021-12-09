1dd9f3df42c46925fc2f7294192d2da906a63220
odrah <papermil@outlook.com.br>
2021-12-09 17:22:00 -0300

    Fomatacao do layout

--- lib/Home.dart
+++ lib/Home.dart
@@ -20,51 +20,69 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.blue,
       ),
       body: Container(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(bottom: 32),
-              child: Image.asset("imagens/logo.png"),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Text(
-                "Saiba qual a melhor opção para o abastecimento do seu carro!",
-                style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 32),
+                child: Image.asset("imagens/logo.png"),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Saiba qual a melhor opção para o abastecimento do seu carro!",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold
+                  ),
                 ),
               ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Preço Alcool, ex: 1.59"
-              ),
-              style: TextStyle(
-                fontSize: 22
-              ),
-              controller: _controllerAlcool,
-            ),
-            TextField(
+              TextField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
-                    labelText: "Preço Gasolina, ex: 3.59"
+                    labelText: "Preço Alcool, ex: 1.59"
                 ),
                 style: TextStyle(
                     fontSize: 22
                 ),
-                controller: _controllerGasolina
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: ElevatedButton(
-                onPressed: (){},
-                child: Text("Calcular")
+                controller: _controllerAlcool,
+              ),
+              TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Preço Gasolina, ex: 3.59"
+                  ),
+                  style: TextStyle(
+                      fontSize: 22
+                  ),
+                  controller: _controllerGasolina
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: ElevatedButton(
+                    onPressed: (){},
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(
+                          fontSize: 20
+                      ),
+                    )
+                )
+                ,),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  "Resultado",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold
+                  ),
+                ),
               )
-              ,),
-          ],
+            ],
+          ),
         ),
       ),
     );
