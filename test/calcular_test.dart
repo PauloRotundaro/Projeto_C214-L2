1b3964d04d64e7f053fe3773459424a2419643f1
Paulo Rotundaro <paulo.rotundaro@toodoo.com.br>
2021-12-11 10:27:56 -0300

    Feat: Previous test edited and new tests added to 'alcool' and 'gasolina' prices

--- test/calcular_test.dart
+++ test/calcular_test.dart
@@ -3,16 +3,24 @@ import 'package:test/test.dart';
 void main(){
   //Função void Calcular
 
-    double? precoAlcool = 4;
-    double? precoGasolina = 7;
+    double? precoAlcool = 4.3;
+    double? precoGasolina = 7.8;
     String _textoResultado = "";
 
-    test('Preço do alcool é nulo', () {
+    test('Preço do álcool é um número positivo válido', () {
       assert(precoAlcool != null && precoAlcool >= 0);
     });
 
-    test('Preço da gasolina é nulo', () {
+    test('Preço da gasolina é um número positivo válido', () {
       assert(precoGasolina != null && precoGasolina >= 0);
+    });
+
+    test('Preço da gasolina é maior que o do álcool', (){
+      assert(precoGasolina != null && precoGasolina >= precoAlcool);
+    });
+
+    test('Preço do alcool é maior que o da gasolina', (){
+      assert(precoAlcool != null && precoAlcool >= precoGasolina);
     });
 
     if( precoAlcool == null || precoGasolina == null ){
